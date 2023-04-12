6d7328df8c1a6fec7d0433119682865462660329
Abdul Manan <abdulmanan@pitb.gov.pk>
2023-04-12 14:57:59 +0500

    merging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
 
-  if (localStorage.getItem("user")) {
+  if (localStorage.getItem("user") != null) {
     const user = JSON.parse(localStorage.getItem("user"));
     console.log("user", user);
   } else {
